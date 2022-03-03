1b737d1529ac196062779476678307f675e2b493
joaomcteixeira <joaomcteixeira@gmail.com>
2022-03-03 13:54:53 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# passgen
-Simple password generator made in Python
+An utterly simple password generator made in Python.
+
+By default, a password contains lower and upper case characters, digits,
+and punctuation. You can disable these different types separately.
+
+**Usage:**
+
+```bash
+    python passgen.py -h
+    python passgen.py
+    python passgen.py -pu
+```
+
+**Example:**
+
+```bash
+$ python passgen.py 
+eTJ1s.n_p,JBRVn?
+```
+
+```bash
+$ python passgen.py -pu -l 10
+6T0dHq2djO
+```
---
+++ passgen.py
@@ -0,0 +1,97 @@
+"""
+An utterly simple password generator made in Python.
+
+By default, a password contains lower and upper case characters, digits,
+and punctuation. You can disable these different types separately.
+
+USAGE:
+    $ python passgen.py -h
+    $ python passgen.py
+    $ python passgen.py -pu
+"""
+import argparse
+import random
+import string
+from contextlib import suppress
+
+
+class _JoinDisable(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        disable = ''.join(chars for d in values for chars in d).replace(" ", "")
+        setattr(namespace, self.dest, disable)
+
+
+ap = argparse.ArgumentParser(description=__doc__)
+
+ap.add_argument('-l', help='Password length. Defaults to 16.', default=16, type=int)
+ap.add_argument('-lo', help='Disable lower case chars.', action="store_false")
+ap.add_argument('-up', help='Disable upper case chars.', action="store_false")
+ap.add_argument('-di', help='Disable digits.', action="store_false")
+ap.add_argument('-pu', help='Disable punctuation.', action="store_false")
+ap.add_argument(
+    '-D',
+    '--disable',
+    help=(
+        'Disable characters. Give a list of characters to disregard. '
+        'For example: --disable "A B u ( )". '
+        'To disable complex punctuation you might need to use the backslash: '
+        '--disable "\`" for example.'
+        ),
+    nargs="*",
+    action=_JoinDisable,
+    )
+
+chars_possibilities = {
+    'lower': string.ascii_lowercase,
+    'upper': string.ascii_uppercase,
+    'digits': string.digits,
+    'punctuation': string.punctuation,
+    }
+
+
+def main(l=16, lo=True, up=True, di=True, pu=True, disable=None):
+    """Create a password."""
+    # maps CLI choices
+    choices = {
+        'lower': lo,
+        'upper': up,
+        'digits': di,
+        'punctuation': pu,
+        }
+
+    CA = chars_possibilities
+
+    # removes disable chars. Simplistic implementation
+    if disable:
+        for char_type, chars in CA.items():
+            char_list = list(chars)
+            for char in disable:
+                with suppress(ValueError):
+                    char_list.remove(char)
+            CA[char_type] = ''.join(char_list)
+
+    # gets one char for each type the user selected
+    pass_chars = []
+    for char_type, user_choice in choices.items():
+        if user_choice:
+            pass_chars.append(random.choice(CA[char_type]))
+            l -= 1
+
+    # all possible chars according to the user choices
+    all_chars = ''.join(opt for typ, opt in CA.items() if choices[typ])
+
+    # chooses chars for the remaining pass length
+    rest_chars = random.choices(all_chars, k=l)
+
+    # joins the first unique-type chars with the random selection
+    password = pass_chars + rest_chars
+
+    # further shuffles
+    random.shuffle(password)
+
+    print(''.join(password))
+
+
+if __name__ == '__main__':
+    args = vars(ap.parse_args())
+    main(**args)
